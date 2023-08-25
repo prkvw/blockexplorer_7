85952658d4b02dcba040cbe449003e9453a15633
prkvw <neeqash@gmail.com>
2023-08-25 13:25:54 +0000

    update

--- src/App.js
+++ src/App.js
@@ -3,20 +3,12 @@ import { useEffect, useState } from 'react';
 
 import './App.css';
 
-// Refer to the README doc for more information about using API
-// keys in client-side code. You should never do this in production
-// level code.
+
 const settings = {
   apiKey: process.env.REACT_APP_ALCHEMY_API_KEY,
-  network: Network.ETH_MAINNET,
+  network: Network.POLYGON_MUMBAI,
 };
 
-
-// In this week's lessons we used ethers.js. Here we are using the
-// Alchemy SDK is an umbrella library with several different packages.
-//
-// You can read more about the packages here:
-//   https://docs.alchemy.com/reference/alchemy-sdk-api-surface-overview#api-surface
 const alchemy = new Alchemy(settings);
 
 function App() {
@@ -33,15 +25,52 @@ function App() {
   return <div className="App">Block Number: {blockNumber}</div>;
 }
 
-export default App;
+const main = async () => {
+  //Assign the hash from the block number property above , then assign to a variable
+  //let txHash = "0x92fc42b9642023f2ee2e88094df80ce87e15d91afa812fef383e6e5cd96e2ed3";
 
+  //Response would return the block for provided hash
+  let response = await alchemy.core.getBlockWithTransactions(txHash);
+
+  //Logging the response to the console
+  //console.log(response)
+  //return <div className="App">Block: {Block}</div>;
+//};
+
+//main();
+const gain = async () => {
+  // This response fetches the balance of the given address in the paramter as of the provided block.
+  let response = await alchemy.core.getBalance("fiyin.eth", "latest")
+
+  //Logging the response to the console
+  console.log(response)
+  //return <div className="App">Block: {Block}</div>;
+};
+gain();
+
+const pain = async () => {
+
+  //Call the method
+  let response = await alchemy.core.getGasPrice()
+
+  //Logging the response to the console
+  console.log(response)
+};
+
+pain();
+}
+//export default App;
 
 // //header ScopeSevenScan Tab  Home - Tokens - FAQ - 
 //search bar - placeholder = address/transaction hash/block
+//autoGenerateBlockNumber
+
+//getHash - getBlock
 //page/address
 
-//overview - getAddress
-// getBalance
 
 //table
 //getHash || Method || Block || Age || getAddress || getDestination || Value || gas Fee
+
+//overview - getAddress
+// getBalance
